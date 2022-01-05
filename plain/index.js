c19b097e6dc8bda5586db183f94edf2da34ed2e2
MarcoThePhoenixX <65812225+MarcoThePhoenixX@users.noreply.github.com>
2022-01-05 19:22:14 +0100

    calculation of noonMeridian (#5)
    * calculation of noonMeridian
    
    * change noonMeridian
    
    * merged to main

--- plain/index.js
+++ plain/index.js
@@ -10,27 +10,11 @@ function triggerResize() {
 
 document.addEventListener("DOMContentLoaded", function () {
     fetch('./example_places.geojson').then(res => res.json()).then(places => {
+        let onInit = true;
+        let lng = 0;
+        let zoom = { altitude: 2.5 };
 
-
-        const arcsData = [
-            {
-                startLat: 0,
-                startLng: 0,
-                endLat: 90,
-                endLng: 0,
-                color: 'red'
-            },
-            {
-                startLat: 0,
-                startLng: 0,
-                endLat: -90,
-                endLng: 0,
-                color: 'red'
-            }
-        ];
-
-        let zoom = {altitude: 2.5};
-        globe = Globe()
+        const globe = Globe()
             .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
             .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
             .width(calcWidth())
@@ -43,12 +27,6 @@ document.addEventListener("DOMContentLoaded", function () {
             .labelDotRadius(d => Math.sqrt(d.properties.pop_max) * 4e-4)
             .labelColor(() => 'rgba(255, 165, 0, 0.75)')
             .labelResolution(2)
-            .arcsData(arcsData)
-            .arcColor('color')
-            .arcAltitude(0)
-            .arcStroke(1)
-            // .arcDashLength(() => Math.random())
-            .arcDashGap(0)
             .showAtmosphere(true)
             (document.getElementById('globe'))
 
@@ -58,9 +36,68 @@ document.addEventListener("DOMContentLoaded", function () {
         globe.onZoom(v => {
             zoom = v;
         })
+        
+        function refresh() {
+            lng = calcNoonMeridian();        
+      
+            const arcsData = [
+                {
+                    startLat: 0,
+                    startLng: lng,
+                    endLat: 90,
+                    endLng: lng,
+                    color: 'red'
+                },
+                {
+                    startLat: 0,
+                    startLng: lng,
+                    endLat: -90,
+                    endLng: lng,
+                    color: 'red'
+                }
+            ];
+        
+            globe
+            .arcsData(arcsData)
+            .arcColor('color')
+            .arcAltitude(0)
+            .arcStroke(0.25)
+            .arcDashGap(0)    
+                   
+            console.log(lng);
+            setTimeout(refresh, 5000);
+        }
+        
+        refresh();
+
+        if(onInit) {
+            globe.pointOfView({ lat: 0, lng: lng+10, altitude: zoom.altitude }, 1000)
+            onInit = false;
+        }
+
         setGlobePOV();
     });
+    
 })
+
+
+
+function calcNoonMeridian() {
+    var nowUtc = moment().utc();
+
+    let timeEquation = calcTimeEquation(nowUtc.dayOfYear()); 
+
+    let currTimeinH = nowUtc.hour() + nowUtc.minute() / 60 + nowUtc.second() / 3600 ;
+
+    let long = (-180/12) * (currTimeinH + timeEquation - 12);
+
+    return long;
+}
+
+function calcTimeEquation(currDay) {
+    let teq = (-0.171)*Math.sin(0.0337 * currDay + 0.465) - 0.1299*Math.sin(0.01787 * currDay - 0.168);
+    return teq
+}
 
 function calcWidth() {
     return window.innerWidth * 1.35;
